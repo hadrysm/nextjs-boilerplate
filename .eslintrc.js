2bf384da62ef501fff19236f31ff9a9ececc1808
Mateusz Hadryś <m.hadrys.95@gmail.com>
2022-06-03 12:00:03 +0200

    Add eslint, prettier settings

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  root: true,
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'eslint-plugin-import-helpers'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'import-helpers/order-imports': [
+      2,
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/^next/',
+          'module',
+          '/^components/',
+          '/^utils/',
+          '/^styles/',
+          ['parent', 'sibling', 'index']
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true }
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
+};
