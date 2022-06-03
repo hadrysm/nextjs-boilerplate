e67d800a7d2a4ad00f209ef0c2c61652c7d20445
Mateusz Hadryś <m.hadrys.95@gmail.com>
2022-06-03 18:35:23 +0200

    feat: Add plop generator

---
+++ .plop/plopfile.js
@@ -0,0 +1,44 @@
+module.exports = (plop) => {
+  plop.setGenerator('component', {
+    description: 'Application Component',
+    prompts: [
+      {
+        type: 'list',
+        name: 'componentType',
+        message: 'What component do you want to create?',
+        choices: ['atoms', 'molecules', 'organisms', 'templates']
+      },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name?'
+      }
+    ],
+    actions: [
+      // index.ts
+      {
+        type: 'add',
+        path: '../src/components/{{componentType}}/{{pascalCase name}}/index.ts',
+        templateFile: 'templates/component/index.ts.hbs'
+      },
+      // component
+      {
+        type: 'add',
+        path: '../src/components/{{componentType}}/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/component/component.tsx.hbs'
+      },
+      // storybook
+      {
+        type: 'add',
+        path: '../src/components/{{componentType}}/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/component/stories.tsx.hbs'
+      },
+      // tests
+      {
+        type: 'add',
+        path: '../src/components/{{componentType}}/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+        templateFile: 'templates/component/test.tsx.hbs'
+      }
+    ]
+  });
+};
--- src/components/atoms/Logo/Logo.stories.tsx
+++ src/components/atoms/Logo/Logo.stories.tsx
@@ -8,6 +8,6 @@ export default {
   component: Logo
 } as ComponentMeta<typeof Logo>;
 
-const Template: ComponentStory<typeof Logo> = (args) => <Logo {...args}>Link content</Logo>;
+const Template: ComponentStory<typeof Logo> = (args) => <Logo {...args} />;
 
 export const Default = Template.bind({});
