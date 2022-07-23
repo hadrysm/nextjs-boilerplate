15b6bea31e6095def1fe0af06280f6a58e625c02
Mateusz Hadryś <62218176+hadrysm@users.noreply.github.com>
2022-07-23 13:36:29 +0200

    feat: Update home page (#121)
    * feat: Update home page
    
    * feat: Update layout for pages
    
    * fix: Fix test for pages
    
    * feat: Update meta title in home page

---
+++ __tests__/custom-layout.test.tsx
@@ -0,0 +1,13 @@
+import CustomLayoutPage from '@/pages/custom-layout';
+import { render, screen } from '@/tests/test-utils';
+
+describe('CustomLayoutPage Page', () => {
+  it('renders a heading', () => {
+    render(<CustomLayoutPage />);
+
+    // Assert
+    screen.getByRole('heading', {
+      name: /Nextjs boilerplate - Custom layout/i
+    });
+  });
+});
--- src/components/templates/MainLayout/MainLayout.tsx
+++ src/components/templates/MainLayout/MainLayout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FC, ReactNode } from 'react';
 
 interface Props {
@@ -6,4 +7,22 @@ interface Props {
 
 // This is the place responsible for wrapping your app.
 // Add here components like Footer, Nav etc.
-export const MainLayout: FC<Props> = ({ children }) => <main>{children}</main>;
+export const MainLayout: FC<Props> = ({ children }) => (
+  <div>
+    <header className="p-4 bg-slate-900">
+      <ul className="flex items-center gap-10 text-gray-50">
+        <li>
+          <Link href="/">
+            <a className="p-2 inline-block transition-colors hover:text-green-300">Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/custom-layout" className="p-2">
+            <a className="p-2 inline-block transition-colors hover:text-green-300">Custom layout</a>
+          </Link>
+        </li>
+      </ul>
+    </header>
+    <main className="bg-[#FCFCFC]">{children}</main>;
+  </div>
+);
