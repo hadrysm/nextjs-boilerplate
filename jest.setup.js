371cc9fa2df82c2bf336b7040b2e57d1941eaeca
Mateusz Hadryś <m.hadrys.95@gmail.com>
2023-11-11 00:19:18 +0100

    feat: Remove yarn & add pnpm & fix build

--- jest.setup.js
+++ jest.setup.js
@@ -4,4 +4,3 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
